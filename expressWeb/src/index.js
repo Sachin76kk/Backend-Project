b2d9eb38751abcf08ef1b06254ac62c22e11addd
Sachin <Sachin76kk@gmail.com>
2023-10-08 22:58:11 +0530

    first commit

---
+++ expressWeb/src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+// public static path
+const staticPath = path.join(__dirname, "../public");
+
+
+app.set("view engine", "hbs");
+app.use(express.static(staticPath));
+
+// routing
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/weather", (req, res) => {
+  res.render("weather");
+});
+app.get("*", (req, res) => {
+  res.render("404",{
+    errorMsg:"Oops! Page not Found"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening to port no https://localhost:${port}`);
+});
